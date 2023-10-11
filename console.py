220fee3b48cf56eb3a640ebe8ca2264c12434cf7
KwameNtaadu007 <kwamealabama@gmail.com>
2023-10-11 13:16:32 -0700

    basic console created

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl+D)"""
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+

a00611661c9c464d0a59e9d25bf46d5da982313c
KwameNtaadu007 <kwamealabama@gmail.com>
2023-10-11 13:57:55 -0700

    created a user class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,10 +3,18 @@
 
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+
+     # Add User to the list of classes
+    _classes = {
+        "BaseModel": BaseModel,
+        "User": User
+    }
 
     def all(self):
         return FileStorage.__objects
@@ -16,11 +24,13 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
         obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
+        """ deserializes the JSON file to __objects """
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
@@ -29,4 +39,5 @@ class FileStorage:
                     obj_cls = eval(class_name)
                     obj = obj_cls(**value)
                     FileStorage.__objects[key] = obj
-
+     
+     
